ec7eaf759f1768d1edb5195d18af0a74b3aea32b
sagarcrestha55-hash <sagarcrestha55@gmail.com>
2026-01-10 09:13:30 +0545

    Starting new python learning series where i learn python everyday till expert

---
+++ day2/variables.py
@@ -0,0 +1,73 @@
+#  Day 2: 30 days of python programming
+
+first_name = "Ram";
+last_name = "Kumar";
+full_name = "Ram Kumar";
+country = "uganda";
+city = "kampala";
+age = 20;
+year = 2026;
+is_married = False;
+is_true = True;
+is_light_on = True;
+a, b, c = 1, 2, 3;
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(a))
+print(type(b))
+print(type(c))
+
+print(len(first_name))
+print(len(last_name))
+print(first_name > last_name)
+
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+diff = num_two - num_one
+product = num_two * num_one
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+print(total)
+print(diff)
+print(product)
+print(division)
+print(remainder)
+print(exp)
+print(floor_division)
+
+radius = 30
+area_of_circle = 3.14* radius**2
+circum_of_circle = 2*3.14*radius
+print(area_of_circle)
+print(circum_of_circle)
+
+
+radius = int(input("Enter the radius of the circle: "))
+area_of_circle = 3.14* radius**2
+print(area_of_circle)
+print(circum_of_circle)
+
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+country = input("Enter your country: ")
+age = int(input("Enter your age: "))
+print(first_name)
+print(last_name)
+print(country)
+print(age)
+
